cde0596b4b00baebbaaab0a758601fe9143f2db7
Abraham Vineel Katikala <abrahamvineel@gmail.com>
2024-04-06 18:55:55 -0400

    used format to concatenate string

--- data_gen/src/main.rs
+++ data_gen/src/main.rs
@@ -1,20 +1,15 @@
 
 use std::fs::File;
-use std::io::{Read, Write};
+use std::io::Write;
 
 fn main() {
+    const RANGE: u32 = 1_000_000;
     let name = String::from("apple");
     let subject = String::from("computer_science");
-    const RANGE: u32 = 1_000_000;
-
     let mut file = File::create("data.txt").expect("Failed to create file");
 
-
     for i in 0..=RANGE {
-        let data = name.clone() + &i.to_string() + "," + &subject.clone() + &i.to_string() + "\n";
-        // println!("{}", data);
+        let data = format!("{}{},{}{}\n", name, i, subject, i);
         file.write_all(data.as_bytes()).expect("Failed to write to file");
-        // println!("{} {} {}", i, name.clone() + &i.to_string(), subject.clone() + &i.to_string());
     }
-
 }
