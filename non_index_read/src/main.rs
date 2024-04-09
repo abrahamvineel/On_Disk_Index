510e696b1dfb886e4855a2e81859f3aa0a207cf2
Abraham Vineel Katikala <abrahamvineel@gmail.com>
2024-04-08 21:22:47 -0400

    non index read wip

--- non_index_read/src/main.rs
+++ non_index_read/src/main.rs
@@ -1,14 +1,36 @@
 use std::fs::File;
 use std::io;
-use std::io::BufRead;
+use std::io::{BufRead, Read};
+use std::thread;
+use std::time::{Instant, Duration};
 
 fn main() {
-    println!("Value is {}", get_value("apple10"))
+    // let start_time = Instant::now();
+
+    // println!("Value is {}", get_value("apple100000000"));
+    // println!("Elapsed time: {:?}", Instant::now() - start_time);
+
+    let mut handles = vec![];
+    for i in 1..=10 {
+        let handle = thread::spawn(move || {
+            // Call a function with different parameters from each thread
+            let v = 100000000 - i;
+            let val = format!("{}{}", "apple", v);
+            let start_time = Instant::now();
+            println!("Value is {}", get_value(&val));
+            println!("Elapsed time: {:?}", Instant::now() - start_time);
+        });
+        handles.push(handle);
+    }
+
+    // Wait for all threads to finish
+    for handle in handles {
+        handle.join().unwrap();
+    }
 }
 
 fn get_value(queried_key:&str) -> String{
     let file = File::open("../data_gen/data.txt").unwrap();
-
     let reader = io::BufReader::new(file);
 
     for line in reader.lines() {
@@ -24,3 +46,25 @@ fn get_value(queried_key:&str) -> String{
     }
     return String::new();
 }
+
+// fn get_value(queried_key:&str) -> String{
+//     let mut file = File::open("../data_gen/data100mil.txt").unwrap();
+//     let mut buffer = String::new();
+//     file.read_to_string(&mut buffer).unwrap();
+//
+//     // let reader = io::BufReader::new(file);
+//
+//     for line in buffer.split("\n") {
+//         // println!("{}", line);
+//         // let res = match line {
+//         //     Ok(line) => String::from(line),
+//         //     Err(err) => String::from(err.to_string())
+//         // };
+//         let row: Vec<&str> = line.split(",").collect();
+//         let (key, value) = (row[0], row[1]);
+//         if key == queried_key {
+//             return value.to_string()
+//         }
+//     }
+//     return String::new();
+// }
---
+++ non_index_read/target/debug/build/libc-05c93818e8576168/build_script_build-05c93818e8576168.d
@@ -0,0 +1,5 @@
+C:\Pro\On_Disk_Index\non_index_read\target\debug\build\libc-05c93818e8576168\build_script_build-05c93818e8576168.exe: C:\Users\abvin\.cargo\registry\src\index.crates.io-6f17d22bba15001f\libc-0.2.153\build.rs
+
+C:\Pro\On_Disk_Index\non_index_read\target\debug\build\libc-05c93818e8576168\build_script_build-05c93818e8576168.d: C:\Users\abvin\.cargo\registry\src\index.crates.io-6f17d22bba15001f\libc-0.2.153\build.rs
+
+C:\Users\abvin\.cargo\registry\src\index.crates.io-6f17d22bba15001f\libc-0.2.153\build.rs:
