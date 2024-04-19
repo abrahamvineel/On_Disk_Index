8555ba2d8b4d5d7c3fd8627cc881a6428668f941
Abraham Vineel Katikala <abrahamvineel@gmail.com>
2024-04-18 20:51:29 -0400

    bplustree index

---
+++ bplustree_index/src/main.rs
@@ -0,0 +1,18 @@
+
+struct Node {
+    keys: Vec<i32>,
+    children: Vec<usize>
+}
+
+struct LeafNode {
+    keys: Vec<i32>,
+    values: Vec<usize>
+}
+
+struct BPlusTree {
+
+}
+
+fn main() {
+
+}
